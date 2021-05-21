0547175a1168bafe7aebc38df96da239f65059ce
alexbogreek <olivesargreek@gmail.com>
2021-05-21 12:58:53 +0300

    modified: js/react.js basic JS

---
+++ js/react.js
@@ -0,0 +1,40 @@
+// if (4 == 4) {
+//     console.log('Ok');
+// } else {
+//     console.log('Error');
+// }
+
+
+// const num = 50;
+
+// if (num < 49 ) {
+//     console.log('Error');
+
+// } else if (num > 100) {
+//     console.log('Слишком много');
+// } else {
+//     console.log('Ok!');
+// }
+
+
+
+// (num === 50) ? console.log("Ok!") : console.log('Error!');
+
+
+const num = 150;
+
+switch (num) {
+    case 49:
+        console.log('Error');
+        break;
+    case 100:
+        console.log('Error');
+        break;
+    case 50: 
+    console.log('Good!');
+    break;
+    default: 
+    console.log('Не в ту дырку!');
+    break;
+}
+
